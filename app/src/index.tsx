6c117c5df2cd69d34eeaf2c5e50b6ff46041dc23
istott <isaac.cloyd@gmail.com>
2022-05-30 16:40:30 -0600

    baseline setup

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
+import { Provider } from 'react-redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { configureStore } from '@reduxjs/toolkit';
+
+const saga = createSagaMiddleware();
+const store = configureStore({
+  reducer: {},
+  middleware: [saga]
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
